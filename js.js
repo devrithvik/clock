901c78d7e128bf7bb29f762b5d2a2c05d31305ce
rithu <rithu@gmail.com>
2023-01-17 15:22:54 +0530

    added the files

---
+++ js.js
@@ -0,0 +1,30 @@
+const hrs=document.getElementById("hrs");
+const min=document.getElementById("min");
+const sec=document.getElementById("sec");
+const ampm=document.getElementById("ampm");
+
+function update_time(){
+
+    h = new Date().getHours();
+    m = new Date().getMinutes();
+    s = new Date().getSeconds();
+    ampm.innerHTML = "AM";
+            
+    if(h>12){
+        h = h-12;
+        ampm.innerHTML = "PM";
+    }
+    if(h==12){
+        ampm.innerHTML = "PM";
+    }
+    if(h==0){
+        h=12;
+        ampm.innerHTML = "AM";
+    }
+     hrs.innerHTML = h;
+     min.innerHTML = m;
+     sec.innerHTML = s;
+     
+     setTimeout(function timeout(){update_time()},1000);
+}
+update_time();
